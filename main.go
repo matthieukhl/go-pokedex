6f7036a7f1a2adcda87923e0a5c1acaae71d9129
Matthieu Khairallah <matthieu.khairallah@ringover.com>
2025-09-03 17:07:54 +0200

    refactored code as described in solution files

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"time"
+
+	"github.com/matthieukhl/go-pokedex/internal/pokeapi"
+)
+
+func main() {
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
+	}
+
+	startRepl(cfg)
+}
