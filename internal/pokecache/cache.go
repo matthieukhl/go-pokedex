c98e02c328b8362be7dd1de492be644ec4192ab5
Matthieu Khairallah <matthieu.khairallah@ringover.com>
2025-09-04 11:32:28 +0200

    created Get and Add method for Cache struct

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -11,18 +11,30 @@ type Cache struct {
 }
 
 type cacheEntry struct {
-	val       []byte    // Raw data we are caching
-	createdAt time.Time // Represents when the entry was created
+	Val       []byte    // Raw data we are caching
+	CreatedAt time.Time // Represents when the entry was created
 }
 
 func NewCache(interval time.Duration) Cache {
 	return Cache{}
 }
 
-func (c *Cache) Add(key string, val []byte) {}
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.Entries[key] = cacheEntry{
+		Val:       val,
+		CreatedAt: time.Now(),
+	}
+}
 
 func (c *Cache) Get(key string) ([]byte, bool) {
-	return nil, false
+	val, exists := c.Entries[key]
+	if !exists {
+		return nil, false
+	}
+
+	return val.Val, false
 }
 
 func (c *Cache) ReapLoop() {}
